7f6c275f71a956cfa8ec55eadcdd127a5e263173
Someone <rgbjkbs@hdbf.ru>
2018-12-26 16:47:52 +0200

    finish

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Task;
+
 class TaskController extends Controller
 {
     /**
@@ -17,4 +19,41 @@ class TaskController extends Controller
   {
     $this->middleware('auth');
   }
+  
+  /**
+ * Отображение списка всех задач пользователя.
+ *
+ * @param  Request  $request
+ * @return Response
+ */
+public function index(Request $request)
+{
+    $tasks=$request->user()->tasks;
+  return view('tasks.index',['tasks'=>$tasks,]);
+}
+
+/**
+ * Создание новой задачи.
+ *
+ * @param  Request  $request
+ * @return Response
+ */
+public function store(Request $request)
+{
+  $this->validate($request, [
+    'name' => 'required|max:255',
+  ]);
+
+  $request->user()->tasks()->create([
+    'name' => $request->name,
+  ]);
+
+  return redirect('/tasks');
+}
+
+public function destroy(Task $task, Request $request){
+    $this->authorize('destroy', $task);
+    $task->delete();
+    return redirect('/tasks');
+}
 }
